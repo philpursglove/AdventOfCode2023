1c4b67f66c31f2780cce5cd230d6801e1cbcc706
Phil Pursglove <philpursglove@users.noreply.github.com>
2023-12-15 10:07:07 +0000

    Day 15 Part 1

---
+++ Advent2023/Day15/Program.cs
@@ -0,0 +1,28 @@
+﻿using AdventUtilities;
+
+List<string> input = File.ReadAllText("input.txt").Split(",").ToList();
+
+int value = 0;
+int sum = 0;
+
+foreach (string s in input)
+{
+    value = Hash(s, 0);
+    Console.WriteLine($"{s}, {value}");
+    sum += value;
+}
+
+Console.WriteLine(sum);
+Console.ReadLine();
+
+int Hash(string code, int currentValue)
+{
+    for (int i = 0; i < code.Length; i++)
+    {
+        currentValue += code[i].ToAscii();
+        currentValue *= 17;
+        currentValue %= 256;
+    }
+
+    return currentValue;
+}
